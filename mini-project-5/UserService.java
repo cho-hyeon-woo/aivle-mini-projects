88896064c3bda5238ac50d254f910a3d0b5a5b3f
HyeonWoo Cho <robert12154937@gmail.com>
2026-06-14 15:08:14 +0900

    Delete mini-project-5 directory

--- mini-project-5/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.aivle.bookapp.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record UserLoginRequest(
-        @NotBlank String loginId,
-        @NotBlank String password
-) {
-}
